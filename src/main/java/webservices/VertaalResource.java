9388afaa1bf4e963e26219e0f823247e0c327607
HUDannyCao <danny.cao@student.hu.nl>
2019-03-12 22:13:57 +0100

    GET en POST services toegevoegd

--- src/main/java/webservices/VertaalResource.java
+++ src/main/java/webservices/VertaalResource.java
@@ -1,5 +1,30 @@
 package webservices;
 
-public class VertaalResource {
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+@Path("/vertaal")
+public class VertaalResource {
+	
+
+	@POST
+    @Produces("application/json")
+    public Response translateSentence(@FormParam("zin") String zin){
+    	
+    	System.out.println(zin);
+        return Response.ok(zin).build();
+	}
+	
+	@GET
+    @Produces("application/json")
+    public Response testGet(){
+    	String zin = "test";
+        return Response.ok(zin).build();
+	}
+	
+	
 }
